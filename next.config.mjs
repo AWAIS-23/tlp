f43bdddc7d2a7e10aae4d3f5e063c016efe3ef9f
awais <muhammadawais65u@gmail.com>
2026-07-05 00:09:30 +0500

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   images: {
     unoptimized: true,
   },
+  output: 'export', // یہ لائن نیٹلی فائی کے لیے سٹیٹک ایچ ٹی ایم ایل جنریٹ کرے گی
 };
 
 export default nextConfig;
